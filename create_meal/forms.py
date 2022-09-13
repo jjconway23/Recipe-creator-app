b87b6d16551da6af840be9436a18cb760fa9e7eb
jjconway23 <jacob.peat01@gmail.com>
2022-09-13 16:36:38 +0000

    creatre form for recipe

--- create_meal/forms.py
+++ create_meal/forms.py
@@ -6,3 +6,9 @@ class IngredientsForm(ModelForm):
     class Meta:
         model = Ingredients
         fields = '__all__'
+
+
+class RecipeForm(ModelForm):
+    class Meta:
+        model = Recipe
+        fields = '__all__'
