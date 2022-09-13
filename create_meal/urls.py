0213e86be7c9614f51f3c5f620e0914815456121
jjconway23 <jacob.peat01@gmail.com>
2022-09-13 16:40:38 +0000

    Add recipe_create to url patterns

--- create_meal/urls.py
+++ create_meal/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('ingredients_list/', views.IngredientsList.as_view(), name='ingredients_list'),
     path('recipe_list/', views.RecipeList.as_view(), name='recipe_list'),
     path('ingredients_list/create/', views.IngredientsCreate.as_view(), name='ingredients_create'),
+    path('recipe_list/create'/ views.RecipeCreate.as_view(), name='recipe_create'),
 ]
