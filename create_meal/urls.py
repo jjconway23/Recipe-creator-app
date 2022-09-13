e57139d3cae34d48fc31ed4c379fe07203820d5f
jjconway23 <jacob.peat01@gmail.com>
2022-09-13 17:56:29 +0000

    Add recipe_delete.html

--- create_meal/urls.py
+++ create_meal/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('recipe_list/create', views.RecipeCreate.as_view(), name='recipe_create'),
     path('ingredients_list/update/<pk>/', views.IngredientsUpdate.as_view(), name='ingredients_update'),
     path('recipe_list/update/<pk>/', views.RecipeUpdate.as_view(), name='recipe_update'),
+    path('ingredients_list/delete/<pk>', views.IngredientsDelete.as_view(), name='ingredients_delete'),
 ]
