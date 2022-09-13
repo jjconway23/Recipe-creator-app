9e233e0454a6a9e626dad7305efd2bb0d83c20f6
jjconway23 <jacob.peat01@gmail.com>
2022-09-13 15:31:07 +0000

    Override get_context_data for recipielist class

--- create_meal/urls.py
+++ create_meal/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('ingredients_list/', views.IngredientsList.as_view(), name='ingredients_list'),
+    path('recipe_list/', views.RecipeList.as_view(), name='recipe_list'),
 ]
--- create_meal/views.py
+++ create_meal/views.py
@@ -2,12 +2,23 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Ingredients, Recipe
 
-
+# ========================= List Views
 def home(request):
     return render(request, 'create_meal/pages/index.html')
 
 
 class IngredientsList(ListView):
     model = Ingredients
-    template_name = 'create_meal/pages/ingredientslist.html'
+    template_name = 'create_meal/pages/ingredients_list.html'
 
+
+class RecipeList(ListView):
+    model = Recipe
+    template_name = 'create_meal/pages/recipe_list.html'
+    
+
+    def get_context_data(self, **kwargs):          
+        context = super().get_context_data(**kwargs)                     
+        all_ingredients = Ingredients.objects.all()
+        context["ingredients"] = all_ingredients
+        return context
