b9f533c87dfa8b8bfaa925f0d9d92a6ac1150123
jjconway23 <jacob.peat01@gmail.com>
2022-09-14 10:23:38 +0000

    Begin styling the recipe cards

--- recipie_creator/settings.py
+++ recipie_creator/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] # was 'STATIC' but failed to load css
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
