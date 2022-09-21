d494e0c6918d3b70f14fc47b60774a6d1baa3835
jjconway23 <jacob.peat01@gmail.com>
2022-09-21 15:03:15 +0000

    Add a few styles for ux purposes

--- create_meal/forms.py
+++ create_meal/forms.py
@@ -1,6 +1,8 @@
 from django.forms import ModelForm
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from .models import Ingredients, Recipe
-
 
 class IngredientsForm(ModelForm):
     class Meta:
@@ -12,3 +14,4 @@ class RecipeForm(ModelForm):
     class Meta:
         model = Recipe
         fields = '__all__'
+
